27966a7b8fca7611854b6357b0a26cddf4c8baa8
cb97jh <sofialucerobareirorecalde6@gmail.com>
2025-06-12 19:33:10 -0300

    Lol

--- ArcadeGames/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ArcadeGames_FILE C:/Users/Asus/Documents/ArcadeGames/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/ArcadeGames.exe)
-set(__QT_DEPLOY_TARGET_ArcadeGames_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ArcadeGames_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
--- ArcadeGames/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/deploy_ArcadeGames_7bf0464135.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/Users/Asus/Documents/ArcadeGames/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/ArcadeGames-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE C:/Users/Asus/Documents/ArcadeGames/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/ArcadeGames.exe
-    GENERATE_QT_CONF
-)
